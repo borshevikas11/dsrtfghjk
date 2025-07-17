add2a6e89f08c3f4bdf810024e7c45fdcd3a8801
borshevikas11 <nasty.panukova2@gmail.com>
2025-07-17 16:58:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'sportlife-v1';
 const urlsToCache = [
-  './',           // Главная страница
-  './index.html',
-  './style.css',
-  './icons/icon-192x192.png'
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-192x192.png'
 ];
 
 self.addEventListener('install', event => {
